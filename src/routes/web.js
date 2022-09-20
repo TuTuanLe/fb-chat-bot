c3b43c77e27054df82f23958afe1f829bd0cbb09
TuTuanLe <tutuanletkpt@gmail.com>
2022-09-20 23:17:31 +0700

    upodate

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,14 +1,18 @@
-import express from 'express';
-import chatbotController from '../controllers/chatbotController';
+import express from "express";
+import homepageController from "../controllers/homepageController";
 
 let router = express.Router();
 
-let initWebRoutes = (app) => {
-    router.get('/', chatbotController.getHomePage);
-    router.get('/webhook', chatbotController.getWebhook);
-    router.post('/webhook', chatbotController.postWebhook);
+let initWebRoutes = (app)=> {
+    router.get("/", homepageController.getHomePage);
+    router.get("/webhook", homepageController.getWebhook);
+    router.post("/webhook", homepageController.postWebhook);
+    router.post("/set-up-profile", homepageController.handleSetupProfile);
+    router.get("/set-up-profile", homepageController.getSetupProfilePage);
 
-    return app.use('/', router);
+    router.get("/info-order", homepageController.getInfoOrderPage);
+    router.post("/set-info-order", homepageController.setInfoOrder);
+    return app.use("/", router);
 };
 
 module.exports = initWebRoutes;
--- src/server.js
+++ src/server.js
@@ -1,19 +1,24 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import viewEngine from './configs/viewEngine';
-import webRoutes from './routes/web';
-require('dotenv').config();
+require("dotenv").config();
+import express from "express";
+import configViewEngine from "./config/viewEngine";
+import initWebRoutes from "./routes/web";
+import bodyParser from "body-parser";
+
+
 let app = express();
 
-viewEngine(app);
-
-webRoutes(app);
-
+//config body-parser to post data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//config view engine
+configViewEngine(app);
+
+//init web routes
+initWebRoutes(app);
 
 let port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-    console.log('App is running at the port :', port);
+    console.log(`Messenger tech shop is running at the port ${port}`);
 });
