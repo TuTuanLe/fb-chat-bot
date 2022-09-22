a923b6c7f9de483f0641676f4ea1557cb3ee7dfe
TuTuanLe <tutuanletkpt@gmail.com>
2022-09-22 22:04:56 +0700

    update

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -90,20 +90,7 @@ let postWebhook = (req, res) => {
 // Handles messages events
 let handleMessage = async (sender_psid, received_message) => {
     //check the incoming message is a quick reply?
-    if (received_message && received_message.quick_reply && received_message.quick_reply.payload) {
-        let payload = received_message.quick_reply.payload;
-        if (payload === "CATEGORIES") {
-            await chatbotService.sendCategories(sender_psid);
-
-        } else if (payload === "LOOKUP_ORDER") {
-            await chatbotService.sendLookupOrder(sender_psid);
-
-        } else if (payload === "TALK_AGENT") {
-            await chatbotService.requestTalkToAgent(sender_psid);
-        }
-
-        return;
-    }
+    console.log("handle message Tutuanle");
 
 
     let response;
@@ -143,6 +130,24 @@ let handleMessage = async (sender_psid, received_message) => {
             }
         }
     }
+
+
+ 
+    if (received_message && received_message.quick_reply && received_message.quick_reply.payload) {
+        let payload = received_message.quick_reply.payload;
+        if (payload === "CATEGORIES") {
+            await chatbotService.sendCategories(sender_psid);
+
+        } else if (payload === "LOOKUP_ORDER") {
+            await chatbotService.sendLookupOrder(sender_psid);
+
+        } else if (payload === "TALK_AGENT") {
+            await chatbotService.requestTalkToAgent(sender_psid);
+        }
+
+        return;
+    }
+
 
     // Sends the response message
     await chatbotService.sendMessage(sender_psid, response);
