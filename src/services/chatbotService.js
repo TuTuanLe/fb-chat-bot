f471d437c97725101a527ad36dc13132d43b6d82
TuTuanLe <tutuanletkpt@gmail.com>
2022-09-23 07:18:03 +0700

    fix request

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -85,13 +85,23 @@ let sendMessage = (sender_psid, response) => {
             //     &access_token=PAGE-ACCESS-TOKEN
             // })
 
-            const uri = "https://graph.facebook.com/v2.6/me/messages",
-                qs= { "access_token": PAGE_ACCESS_TOKEN },
-                method="POST",
-                json=request_body;
+   
 
             request({
-                uri
+                uri: `https://graph.facebook.com/v15.0/me/messages?access_token: ${PAGE_ACCESS_TOKEN }`,
+                headers: {
+                    'Content-Type': 'application/json',
+                  },
+                method: "POST",
+                body: JSON.stringify({
+                    messaging_type: 'RESPONSE',
+                    recipient: {
+                      id: sender_psid,
+                    },
+                    message: {
+                        response,
+                    },
+                  }),
             }, (err, res, body) => {
                 if (!err) {
                     console.log("message sent!")
